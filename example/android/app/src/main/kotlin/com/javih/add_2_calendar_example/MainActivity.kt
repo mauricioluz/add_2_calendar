3b7045e16f206124758ae6a43c9202b63bc4b633
Mauricio barros <mau.barros@outlook.com>
2024-04-01 11:10:21 -0300

    package example update

--- example/android/app/src/main/kotlin/com/javih/add_2_calendar_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.javih.add_2_calendar_new_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/android/app/src/main/kotlin/com/javih/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.javih.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
